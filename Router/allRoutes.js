e657bba4004653ff7318263190c8c02066353196
MattDaemon1 <matt@mattkonnect.com>
2024-02-20 23:32:00 +0100

    header/footer

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,12 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),;
+    new Route("/projets", "Projets", "/pages/projets.html"),;
+    new Route("/apropos", "A propos", "/pages/apropos.html"),;
+    new Route("/contact", "Contact", "/pages/contact.html"),;
+    new Route("/moncompte", "Mon compte", "/pages/moncompte.html"),;
+    new Route("/login", "Connexion/Deconnexion", "/pages/login.html"),];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Allo la Terre";
