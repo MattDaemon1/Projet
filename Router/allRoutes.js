aa605716b1590995bcdb5dafb9fb701b9573dd4b
MattDaemon1 <matt@mattkonnect.com>
2024-02-22 21:44:12 +0100

    add styles

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,11 +3,11 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),];
-    new Route("/projets", "Projets", "/pages/projets.html"),;
-    new Route("/apropos", "A propos", "/pages/apropos.html"),;
-    new Route("/contact", "Contact", "/pages/contact.html"),;
-    new Route("/moncompte", "Mon compte", "/pages/moncompte.html"),;
-    new Route("/login", "Connexion/Deconnexion", "/pages/login.html"),;]
+new Route("/projets", "Projets", "/pages/projets.html"),;
+new Route("/apropos", "A propos", "/pages/apropos.html"),;
+new Route("/contact", "Contact", "/pages/contact.html"),;
+new Route("/moncompte", "Mon compte", "/pages/moncompte.html"),;
+new Route("/signin", "Connexion", "/pages/signin.html"),;]
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Allo la Terre";
