c7e6573b1d1badd83b8fe3540c36519b912a4a08
MattDaemon1 <matt@mattkonnect.com>
2024-02-25 21:15:18 +0100

    Ajout vérification des champs du formulaire d'inscription

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -6,9 +6,9 @@ export const allRoutes = [
     new Route("/projets", "Projets", "/pages/projets.html"),
     new Route("/apropos", "A propos", "/pages/apropos.html"),
     new Route("/contact", "Contact", "/pages/contact.html"),
-    new Route("/moncompte", "Mon compte", "/pages/moncompte.html"),
+    new Route("/moncompte", "Mon compte", "/pages/Auth/moncompte.html"),
     new Route("/signin", "Connexion", "/pages/Auth/signin.html"),
-    new Route("/signup", "Inscription", "/pages/Auth/signup.html"),
+    new Route("/signup", "Inscription", "/pages/Auth/signup.html", "/JS/auth/signup.js"),
     new Route("/mesdons", "Mes dons", "/pages/Dons/mesdons.html"),
     new Route("/don", "Don", "/pages/Dons/don.html"),
 ];
