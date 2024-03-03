8e1f8f989847500cb82fdcf66b65e798a95af9bf
MattDaemon1 <matt@mattkonnect.com>
2024-03-03 17:58:26 +0100

    Modification Autorize

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -7,9 +7,9 @@ export const allRoutes = [
     new Route("/apropos", "A propos", "/pages/apropos.html", []),
     new Route("/moncompte", "Mon compte", "/pages/Auth/moncompte.html", ["client", "admin"]),
     new Route("/editPassword", "Modifier mot de passe", "/pages/Auth/editPassword.html", ["client", "admin"]),
-    new Route("/signin", "Connexion", "/pages/Auth/signin.html", "/JS/auth/signin.js", ["disconnected"]),
-    new Route("/signup", "Inscription", "/pages/Auth/signup.html", "/JS/auth/signup.js", ["disconnected"]),
-    new Route("/mesdons", "Mes dons", "/pages/Dons/mesdons.html"["client"]),
+    new Route("/signin", "Connexion", "/pages/Auth/signin.html", ["disconnected"], "/JS/auth/signin.js"),
+    new Route("/signup", "Inscription", "/pages/Auth/signup.html", ["disconnected"], "/JS/auth/signup.js"),
+    new Route("/mesdons", "Mes dons", "/pages/Dons/mesdons.html", ["client", "admin"]),
     new Route("/don", "Don", "/pages/Dons/don.html", ["client"]),
 ];
 
